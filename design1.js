40642dd535f7932c1cb9ee482bc9ebd65dd27662
Ravuri Teja Nagaswi <111522542+Tejaswi0418@users.noreply.github.com>
2023-07-28 19:14:06 +0530

    Add files via upload

---
+++ design1.js
@@ -0,0 +1,34 @@
+ReactDOM.render(
+    <div align="center">
+        <h1 align="center">VIRAT KOHLI( INDIAN CRICKETER)</h1>
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj9AW2czAFUyyiRdpk4WtfbL8PG_kFFIQrdg&usqp=CAU" width="300px" height="300px" align="center"></img>      
+    <h2><b><u> Personal Information </u></b></h2>
+    <p><b>Born : </b>5 November 1988(age 34)</p>
+    <p><b>Nick Name : </b>Cheeku,<sup>[1]</sup>King Kohli<sup>[2]</sup></p>
+    <p><b>Hometown : </b>Delhi,India</p>
+    <p><b>Sch0ol : </b>Vishal Bharathi public school,Delhi</p>
+    <p>                 Saviour Convent scnior secondary school,paschimi vihar , New Delhi</p>
+    <p><b>Height : </b>5 ft 9 in(175cm)<sup>[3]</sup></p>
+    <p><b>Weight : </b>69 kg</p>
+    <p><b>Profession : </b> Indian cricketer</p>
+    <p><b>Bowling : </b>Right-arm medium</p>
+    <p><b>Role : </b>Top-order batter</p>
+    <p><b>Spouse : </b>Anushka Sharma(Wife)</p>
+    <p><b>Number of Centuries :</b> 74</p>
+    <p><b>Nationality : </b> Indian</p>
+    <h2><b><u>International Information</u></b></h2>
+    <p><b>National side : </b> India(2008-present)</p>
+    <p><b>Test debut(cap 269): </b>20 june 2011 v West Indies</p>
+    <p><b>Latest Test : </b>20 july 2023 v Australia</p>
+    <p><b>ODI shirt no : </b>18</p>
+    <p><b>Last T20I : </b> List of india Twenty 20 Internatioal cricketers</p>
+    <p><b>T20I shirt no : </b> 18</p>
+    <h2><u><b>Domestic team Information</b></u></h2>
+    <p><b>Years</b>               <b>Team</b></p>
+    <p>2006-present                  Delhi</p> 
+    <p>200-present                   Royal Challenges Banglore</p>
+    </div>
+     
+    ,
+    document.getElementById("biography"));
+    
